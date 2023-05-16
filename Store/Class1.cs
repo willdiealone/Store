b5a94e446ebb440c0a96fe0cae707b4746fac9f5
Dэnnis <109353595+willdiealone@users.noreply.github.com>
2023-05-16 18:30:13 +0400

    создание класса Book

---
+++ Store/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace Store;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+
+    public Book(int Id,string Title)
+    {
+        this.Id = Id;
+        this.Title = Title;
+    }
+}
